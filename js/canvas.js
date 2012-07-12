f788376812cde5d311c659975b0064ebbb0fa0bf
Kevin Baugh <Kevin.Baugh@ucf.edu>
2012-07-12 11:12:30 -0400

    added scoring

--- js/canvas.js
+++ js/canvas.js
@@ -3,15 +3,28 @@ function draw()
 	clear();
 	circle(x, y, 10);
 
-
-	if(x + dx + 10 > WIDTH || x + dx - 10 < 0)
-		dx = -dx-1;
-	if(y + dy + 10 > HEIGHT || y + dy - 10 < 0)
-		dy = -dy;
-
-	x += dx;
-	y += dy;
+	if(timerOffset == 0)
+	{
+		if(x + dx + 10 > WIDTH || x + dx - 10 < 0)
+			dx = -dx-1;
+		if(y + dy - 10 < 0)
+			dy = -dy;
+		if(y + dy + 10 > HEIGHT)
+		{
+			playerDie();
+		}
+		else
+		{
+			x += dx;
+			y += dy;		
+		}
+	}
+	else
+	{
+		timerOffset--;
+	}
+	console.log(timerOffset);
 }
 $(document).ready(function() {  
-	init();
+	interval = init();
 });
--- js/library.js
+++ js/library.js
@@ -3,15 +3,20 @@ var x = 150;
 var y = 150;
 var dx = 2;
 var dy = 4;
+var lives = 3;
 var WIDTH;
 var HEIGHT;
 var ctx;
+var gameover = false;
+var interval;
+var timerOffset = 10;
 
 function init()
 {
 	ctx = $('#canvas')[0].getContext('2d');
 	WIDTH = $('#canvas').width();
 	HEIGHT = $('#canvas').height();
+	$('#lives').html(lives);
 	return setInterval(draw, 10);
 }
 
@@ -34,6 +39,29 @@ function rect(x, y, w, h)
 function clear()
 {
 	ctx.clearRect(0, 0, WIDTH, HEIGHT);
+}
+
+function gameOver()
+{
+	clearInterval(interval);
+	alert('Game Over');
+}
+
+function playerDie()
+{
+	lives -= 1;
+	$('#lives').html(lives);
+	if(lives > 0)
+	{
+		x = 150;
+		y = 150;
+		timerOffset = 10;
+	}
+	else
+	{
+		gameOver();
+	}
+
 }
 
 //END LIBRARY CODE
