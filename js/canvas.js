28f3a26bf9bf48631aade893c4e4fb9cf0473bbd
Dan Brinkman <djbrink@ucf.edu>
2012-07-12 11:08:56 -0400

    added the paddle and ball collision

--- js/canvas.js
+++ js/canvas.js
@@ -2,16 +2,49 @@ function draw()
 {
 	clear();
 	circle(x, y, 10);
-
+	rect(paddle.x, paddle.y, paddle.width, paddle.height);
 
 	if(x + dx + 10 > WIDTH || x + dx - 10 < 0)
 		dx = -dx-1;
 	if(y + dy + 10 > HEIGHT || y + dy - 10 < 0)
 		dy = -dy;
-
+	if(left && paddle.x - paddle.speed > 0)
+	{
+		paddle.x -= paddle.speed;
+	}
+	else if(right && paddle.x + paddle.speed + paddle.width < WIDTH)
+	{
+		paddle.x += paddle.speed;
+	}
+	if(paddleToBallCollide())
+	{
+		dy *= -1;
+	}
 	x += dx;
 	y += dy;
 }
 $(document).ready(function() {  
 	init();
+	//Sets flags for which key is being held down to be used in the Draw()
+	$(document).keydown(function(e) {
+		if(e.keyCode==37 || e.keyCode==65) //left
+		{
+			left = true
+		}
+		if(e.keyCode==39 || e.keyCode==68) //right
+		{
+			right = true;
+		}
+	});
+		//Sets flags for which key is being held down to be used in the Draw()
+	$(document).keyup(function(e) {
+		if(e.keyCode==37 || e.keyCode==65) //left
+		{
+			left = false;
+		}
+		if(e.keyCode==39 || e.keyCode==68) //right
+		{
+			right = false;
+		}
+	});
 });
--- js/library.js
+++ js/library.js
@@ -7,11 +7,23 @@ var WIDTH;
 var HEIGHT;
 var ctx;
 
+//Paddle
+var paddle = new Object();
+
+
+var left = false, right = false;
 function init()
 {
 	ctx = $('#canvas')[0].getContext('2d');
 	WIDTH = $('#canvas').width();
 	HEIGHT = $('#canvas').height();
+
+	paddle.width = 100;
+	paddle.height = 35;	
+	paddle.x = 0;
+	paddle.y = HEIGHT-paddle.height;
+	paddle.speed = 5;
+	paddle.rescale = 1.0;
 	return setInterval(draw, 10);
 }
 
@@ -30,7 +42,13 @@ function rect(x, y, w, h)
 	ctx.closePath();
 	ctx.fill();
 }
-
+function paddleToBallCollide()
+{
+	if(x + 10 > paddle.x && x - 10 < paddle.x + paddle.width &&
+	y + 10 > paddle.y && y - 10 < paddle.y + paddle.height)
+		return true;
+	return false;
+}
 function clear()
 {
 	ctx.clearRect(0, 0, WIDTH, HEIGHT);
