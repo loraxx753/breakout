8febe5c6c98d2c275b5f49fe6b445f55e31241d8
Dan Brinkman <djbrink@ucf.edu>
2012-07-12 11:21:05 -0400

    added paddle, checked for ball collision

--- js/canvas.js
+++ js/canvas.js
@@ -2,7 +2,7 @@ function draw()
 {
 	clear();
 	circle(x, y, 10);
-
+	rect(paddle.x, paddle.y, paddle.width, paddle.height);
 	if(timerOffset == 0)
 	{
 		if(x + dx + 10 > WIDTH || x + dx - 10 < 0)
@@ -18,13 +18,50 @@ function draw()
 			x += dx;
 			y += dy;		
 		}
+		if(left && paddle.x - paddle.speed > 0)
+		{
+			paddle.x -= paddle.speed;
+		}
+		else if(right && paddle.x + paddle.speed + paddle.width < WIDTH)
+		{
+			paddle.x += paddle.speed;
+		}
+		if(paddleToBallCollide())
+		{
+			dy *= -1;
+		}
 	}
 	else
 	{
 		timerOffset--;
 	}
-	console.log(timerOffset);
+		
+		
+		
+		
 }
 $(document).ready(function() {  
 	interval = init();
+	//Sets flags for which key is being held down to be used in the Draw()
+	$(document).keydown(function(e) {
+		if(e.keyCode==37 || e.keyCode==65) //left
+		{
+			left = true
+		}
+		if(e.keyCode==39 || e.keyCode==68) //right
+		{
+			right = true;
+		}
+	});
+		//Sets flags for which key is being held down to be used in the Draw()
+	$(document).keyup(function(e) {
+		if(e.keyCode==37 || e.keyCode==65) //left
+		{
+			left = false;
+		}
+		if(e.keyCode==39 || e.keyCode==68) //right
+		{
+			right = false;
+		}
+	});
 });
--- js/library.js
+++ js/library.js
@@ -11,11 +11,22 @@ var gameover = false;
 var interval;
 var timerOffset = 10;
 
+//Paddle
+var paddle = new Object();
+
+
+var left = false, right = false;
 function init()
 {
 	ctx = $('#canvas')[0].getContext('2d');
 	WIDTH = $('#canvas').width();
 	HEIGHT = $('#canvas').height();
+	paddle.width = 100;
+	paddle.height = 35;	
+	paddle.x = 0;
+	paddle.y = HEIGHT-paddle.height;
+	paddle.speed = 5;
+	paddle.rescale = 1.0;
 	$('#lives').html(lives);
 	return setInterval(draw, 10);
 }
@@ -35,7 +46,13 @@ function rect(x, y, w, h)
 	ctx.closePath();
 	ctx.fill();
 }
-
+function paddleToBallCollide()
+{
+	if(x + 10 > paddle.x && x - 10 < paddle.x + paddle.width &&
+	y + 10 > paddle.y && y - 10 < paddle.y + paddle.height)
+		return true;
+	return false;
+}
 function clear()
 {
 	ctx.clearRect(0, 0, WIDTH, HEIGHT);
